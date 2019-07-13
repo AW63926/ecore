1beb6947ccfba26d37fd11921223d5b2718aab66
AW63926 <aw63926@gmail.com>
2019-07-13 14:46:17 -0400

    initial commit

---
+++ src/main/java/org/ecore/ecore/EcoreApplication.java
@@ -0,0 +1,13 @@
+package org.ecore.ecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoreApplication.class, args);
+	}
+
+}
