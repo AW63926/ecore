4434ad2e8d016175e4be9c5619aea3b7ad29d6c7
kelleyryanj <kelley.ryanj@gmail.com>
2019-07-14 14:17:21 -0400

    added in community member

---
+++ src/main/java/org/ecore/controller/CommunityMemberContoller.java
@@ -0,0 +1,43 @@
+package org.ecore.controller;
+
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.ecore.model.CommunityMember;
+import org.ecore.notFoundException.CommunityMembersNotFoundException;
+import org.ecore.repository.CommunityMemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CommunityMemberContoller {
+
+	@Resource
+	CommunityMemberRepository communityMemberRepo;
+
+	@RequestMapping("/community-member")
+	public String findOneCommunityMember(@RequestParam(value = "id") long id, Model model)
+			throws CommunityMembersNotFoundException {
+		Optional<CommunityMember> communityMember = communityMemberRepo.findById(id);
+
+		if (communityMember.isPresent()) {
+			model.addAttribute("community-members", communityMember.get());
+			return "community-member";
+
+		}
+
+		throw new CommunityMembersNotFoundException();
+
+	}
+
+	@RequestMapping("/all-community-members")
+	public String FindAllCommunityMembers(Model model) {
+		model.addAttribute("community-members", communityMemberRepo.findAll());
+		return "all-community-members";
+
+	}
+
+}
---
+++ src/main/java/org/ecore/notFoundException/CommunityMembersNotFoundException.java
@@ -0,0 +1,5 @@
+package org.ecore.notFoundException;
+
+public class CommunityMembersNotFoundException extends Exception {
+
+}
