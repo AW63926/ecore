03baf334b11e1298aa881441cadee671574021d4
klin2006 <klin2006@gmail.com>
2019-07-13 21:51:21 -0400

    Teachers JPA Mappings Test

---
+++ src/test/java/org/ecore/ecore/TeacherRepository.java
@@ -0,0 +1,7 @@
+package org.ecore.ecore;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeacherRepository extends CrudRepository<Teacher, Long> {
+
+}
---
+++ src/test/java/org/ecore/ecore/TeachersJPAMappingsTest.java
@@ -0,0 +1,52 @@
+package org.ecore.ecore;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class TeachersJPAMappingsTest {
+	
+	@Resource
+	private TestEntityManager entityManager;
+	
+	@Resource
+	private TeacherRepository teacherRepo;
+	
+	@Test
+	public void shouldSaveAndLoadTeacher() {
+		Teacher teacher = teacherRepo.save(new Teacher("name", "school"));
+		long teacherId = teacher.getId();
+		
+		entityManager.flush();
+		entityManager.clear();
+		
+		Optional<Teacher> result = teacherRepo.findById(teacherId);
+		teacher = result.get();
+		assertThat(teacher.getName(), is("name"));
+		
+	}
+	
+	@Test
+	public void shouldGenerateTeacherId() {
+		Teacher teacher = teacherRepo.save(new Teacher("name", "school"));
+		long teacherId = teacher.getId();
+		
+		entityManager.flush();
+		entityManager.clear();
+		
+		assertThat(teacherId, is(greaterThan(0L)));
+	}
+
+}
