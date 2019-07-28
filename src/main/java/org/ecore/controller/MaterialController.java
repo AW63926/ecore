dc143b4d76d8403bcd907eede78adf1ea05f992a
AW63926 <aw63926@gmail.com>
2019-07-28 16:38:45 -0400

    non working file upload feature

--- src/main/java/org/ecore/controller/MaterialController.java
+++ src/main/java/org/ecore/controller/MaterialController.java
@@ -4,20 +4,29 @@ import java.util.Optional;
 
 import javax.annotation.Resource;
 
+import org.ecore.filestorage.StorageService;
 import org.ecore.model.Material;
 import org.ecore.model.Tag;
 import org.ecore.notFoundException.MaterialNotFoundException;
 import org.ecore.repository.MaterialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MaterialController {
 
 	@Resource
 	MaterialRepository materialRepo;
+	
+	@Autowired
+	StorageService storageService;
+	
+	
 
 	@RequestMapping("/material")
 	public String findOneMaterial(@RequestParam(value = "id") long id, Model model) throws MaterialNotFoundException {
@@ -39,14 +48,19 @@ public class MaterialController {
 	}
 
 	@RequestMapping("/add-material")
-	public String addMaterial(String name, int quantity, String descMaterial) {
+	public String addMaterial(@RequestParam("file") MultipartFile file, String name, int quantity, String descMaterial,RedirectAttributes redirectAttributes) {
 		Material material = materialRepo.findByName(name);
 
 		if (material == null) {
+
 			material = materialRepo.save(new Material(name, quantity, descMaterial));
 
 		}
-
+		material.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+		
+		
 		return "redirect:/all-materials";
 	}
 
--- src/main/java/org/ecore/model/Material.java
+++ src/main/java/org/ecore/model/Material.java
@@ -1,18 +1,30 @@
 package org.ecore.model;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.stream.Stream;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import org.ecore.filestorage.StorageException;
+import org.ecore.filestorage.StorageService;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
 
 
 @Entity
-public class Material {
+public class Material implements StorageService{
 	
 	@GeneratedValue	
 	@Id
@@ -23,10 +35,12 @@ public class Material {
 	
 	private String descMaterial;
 	
+	private Path rootLocation;
+	
 	@ManyToMany
 	private Collection<Tag> tags;
 
-	public Material(String name, int quantity, String descMaterial, Tag...tags) {
+	public Material(String name, int quantity, String descMaterial,Tag...tags) {
 		this.name = name;
 		this.quantity = quantity;
 		this.descMaterial = descMaterial;
@@ -34,9 +48,7 @@ public class Material {
 		
 	}
 	
-	public Material() {
-		
-	}
+	
 
 	public long getId() {
 		
@@ -84,5 +96,60 @@ public class Material {
 			return false;
 		return true;
 	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	@Override
+	public Stream<Path> loadAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Path load(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource loadAsResource(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	 @Override
+	    public void store(MultipartFile file) {
+	        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+	        try {
+	            if (file.isEmpty()) {
+	                throw new StorageException("Failed to store empty file " + filename);
+	            }
+	            if (filename.contains("..")) {
+	                // This is a security check
+	                throw new StorageException(
+	                        "Cannot store file with relative path outside current directory "
+	                                + filename);
+	            }
+	            try (InputStream inputStream = file.getInputStream()) {
+	                Files.copy(inputStream, this.rootLocation.resolve(filename),
+	                    StandardCopyOption.REPLACE_EXISTING);
+	            }
+	        }
+	        catch (IOException e) {
+	            throw new StorageException("Failed to store file " + filename, e);
+	        }
+	    }
 
 }
