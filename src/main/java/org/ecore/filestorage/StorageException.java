0ef2a94faa1645937bbe825c92ae962f7407cace
AW63926 <aw63926@gmail.com>
2019-08-10 15:30:41 -0400

    test push

--- src/main/java/org/ecore/filestorage/StorageException.java
+++ src/main/java/org/ecore/filestorage/StorageException.java
@@ -5,6 +5,7 @@ public class StorageException extends RuntimeException {
     public StorageException(String message) {
         super(message);
     }
+    //test
 
     public StorageException(String message, Throwable cause) {
         super(message, cause);
