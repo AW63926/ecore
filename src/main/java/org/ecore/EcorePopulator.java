963f4c389298056e5c19e44d0c8d4ef68f5248fa
kelleyryanj <kelley.ryanj@gmail.com>
2019-08-16 20:43:00 -0400

    disabled map url in schools

--- src/main/java/org/ecore/EcorePopulator.java
+++ src/main/java/org/ecore/EcorePopulator.java
@@ -67,7 +67,7 @@ public class EcorePopulator implements CommandLineRunner{
 
 
 		Need need1 = needRepo.save(new Need("Books for Middleboro", 1, "Books", teacher1, tag));
-		Need need2 = needRepo.save(new Need("basketballs for Wayne COunty", 1, "Basketballs", teacher2, tag));
+		Need need2 = needRepo.save(new Need("Basketballs for Wayne County", 1, "Basketballs", teacher2, tag));
 
 		
 		Material material1 = materialRepo.save(new Material("english tests", 1, "download", teacher1, tag));
